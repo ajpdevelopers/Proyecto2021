086debdca1f9b3817b1231e58cab427b254d4d98
Alejo Nievas <alejonievas3@gmail.com>
2021-09-06 18:44:48 -0300

    Conexion

---
+++ Datos/LoginCache.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public static class UserCache
+    {
+        public static int cedula { get; set; }
+        public static string usuario { get; set; }
+        public static string contraseña { get; set; }
+        public static string nombre { get; set; }
+        public static string apellido { get; set; }
+        public static string grupo { get; set; }
+        
+     
+    }
+    
+
+   
+}
