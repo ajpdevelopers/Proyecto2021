0ddba2546544e2be63749445a0d12b285d37d8e2
ajpdevelopers <ajpdev2021@gmail.com>
2021-11-05 01:01:40 -0300

    test

---
+++ TestUnitarios/UnitTest2.cs
@@ -0,0 +1,21 @@
+﻿using Dominio;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestUnitarios
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod2()
+        {
+        
+
+        }
+    }
+}
+
+
+
+
